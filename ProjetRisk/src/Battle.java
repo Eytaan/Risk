94aeac0b0c5a76a9243e3b3d8f865a2ab6fb74b5
Randy <randy.arnold@hotmail.fr>
2018-05-25 12:17:37 +0200

    Constructeur batailles

--- ProjetRisk/src/Battle.java
+++ ProjetRisk/src/Battle.java
@@ -1,4 +1,23 @@
 
 public class Battle {
+	Territory attacker;
+	Territory defender;
+	int att_units;
+	boolean victory;
+	public Battle(Territory attacker, Territory defender, int att_units, int def_units) {
+		this.attacker=attacker;
+		this.defender=defender;
+		this.att_units=att_units;
+	}
+	
+	//METHODS
+	
+	public boolean attPossible(Territory attacker, Territory defender, int att_units) {
+		if (attacker.isNeighbor(defender) && attacker.getUnits()>att_units) {
+			return true;
+		}
+		return false;
+	}
+	
 
 }
