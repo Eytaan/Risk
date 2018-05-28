36cd141c9a48f09a02b6610bb9549829fbdb65ce
Randy <randy.arnold@hotmail.fr>
2018-05-28 15:45:21 +0200

    Gamer classe

--- ProjetRisk/src/Game.java
+++ ProjetRisk/src/Game.java
@@ -1,4 +1,12 @@
 
 public class Game {
+	int player_integer;
+	int ia_integer;
+	boolean victory;
+	public Game(int player_integer, int ia_integer) {
+		this.ia_integer=ia_integer;
+		this.player_integer=player_integer;
+		this.victory = false;
+	}
 
 }
